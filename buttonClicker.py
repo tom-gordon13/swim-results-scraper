816bd766c09905d69c3ef2dfff0192710a75450e
tom-gordon13 <tombcgordon@gmail.com>
2023-05-09 21:12:05 -0600

    Add selenium_package file

--- buttonClicker.py
+++ buttonClicker.py
@@ -1,7 +1,4 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium_packages import *
 import re
 from time import sleep
 
